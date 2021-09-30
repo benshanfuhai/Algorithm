3dfa58a21422acce2385c1f82f000bf89e834648
benshanfuhai <2628254294@qq.com>
2021-09-30 15:48:52 +0800

    T0034

---
+++ src/pers/wc/leetcode/T0034.java
@@ -0,0 +1,56 @@
+package pers.wc.leetcode;
+
+import java.util.Arrays;
+
+public class T0034 {
+
+    public static void main(String[] args) {
+        int[] nums = {};
+        int target = 0;
+        System.out.println(binarySearch(nums, target));
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int index = binarySearch(nums, target);
+        // 若不存在该元素，直接返回[-1,-1]
+        if (index == -1) {
+            return new int[]{-1, -1};
+        }
+        // 存在该元素，找边界
+        int left = index;
+        int right = index;
+        // 找左边界
+        while (left - 1 >= 0 && nums[left - 1] == target) {
+            left--;
+        }
+        // 找右边界
+        while (right + 1 <= nums.length - 1 && nums[right + 1] == target) {
+            right++;
+        }
+        return new int[]{left, right};
+    }
+
+    /**
+     * 二分查找
+     *
+     * @param nums   一维数组
+     * @param target 目标元素
+     * @return 目标元素在数组中的位置，若不存在目标元素，则返回 -1。
+     */
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
