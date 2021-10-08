e6026aa791ef757dee9be39df2a6c8b6197e3c8a
benshanfuhai <2628254294@qq.com>
2021-10-08 09:46:44 +0800

    ListNode

---
+++ src/pers/wc/leetcode/utils/ListNode.java
@@ -0,0 +1,22 @@
+package pers.wc.leetcode.utils;
+
+/**
+ * 单链表
+ */
+public class ListNode {
+    int val;
+
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
