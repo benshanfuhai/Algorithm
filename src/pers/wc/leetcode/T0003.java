f52d6b8baf688543a9e27235498d16a758e1da98
benshanfuhai <2628254294@qq.com>
2021-10-28 10:42:22 +0800

    T0003

--- src/pers/wc/leetcode/T0003.java
+++ src/pers/wc/leetcode/T0003.java
@@ -5,8 +5,9 @@ import java.util.HashMap;
 public class T0003 {
     public static void main(String[] args) {
         // String s = "abcabcbb";
-        String s = "pwwkew";
+        // String s = "pwwkew";
         // String s = "a";
+        String s = "abba";
         // 3
         System.out.println(lengthOfLongestSubstring(s));
     }
@@ -23,11 +24,14 @@ public class T0003 {
             return 0;
 
         }
+        // Key: s中的字符; Value: 字符的下标，在移动left的时候起作用
         HashMap<Character, Integer> map = new HashMap<>();
         int max = 0;
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
+            // 如果map中存在字符，则移动left到该字符的右边
             if (map.containsKey(s.charAt(right))) {
+                // 取max,因为left可能已经移到了较大的位置，如果之后再遇到了index较小的相同的字符，需要移动到此时最大的left值，如abba
                 left = Math.max(left, map.get(s.charAt(right)) + 1);
             }
             map.put(s.charAt(right), right);
