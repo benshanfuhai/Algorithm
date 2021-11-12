0596751401d238803637ee4f2e7daadd30eb297f
benshanfuhai <2628254294@qq.com>
2021-11-12 10:06:41 +0800

    Add TreeNode.

---
+++ src/pers/wc/leetcode/utils/TreeNode.java
@@ -0,0 +1,22 @@
+package pers.wc.leetcode.utils;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode next;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right, TreeNode next) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        this.next = next;
+    }
+}
