ae053d26cfc26275b72621b7293e9f8080f52527
benshanfuhai <2628254294@qq.com>
2022-02-06 14:37:37 +0800

    T0191

---
+++ src/pers/wc/leetcode/T0191.java
@@ -0,0 +1,28 @@
+package pers.wc.leetcode;
+
+public class T0191 {
+    public static void main(String[] args) {
+        // 二进制形式的数
+        int n = 0b00000000000000000000000000001011;
+        System.out.println(n);
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(hammingWeight(n));
+    }
+
+    /**
+     * 位 1 的个数（汉明重量）
+     *
+     * @param n 无符号整数，以二进制串的形式
+     * @return 返回二进制串中 1 的个数
+     */
+    public static int hammingWeight(int n) {
+        String binaryString = Integer.toBinaryString(n);
+        int count = 0;
+        for (int i = 0; i < binaryString.length(); i++) {
+            if (binaryString.charAt(i) == '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
